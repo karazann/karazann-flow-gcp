696e14a25f9bf19072cfd6645eb71313e58fc85f
Sz. Kovács Roland <szkabaroli@gmail.com>
2019-11-17 10:59:14 +0100

    stackdriver logging

--- src/logger.ts
+++ src/logger.ts
@@ -3,6 +3,22 @@ import { LoggingWinston } from '@google-cloud/logging-winston'
 
 const loggingWinston = new LoggingWinston()
 
+// Complete a structured log entry.
+const entry = Object.assign(
+    {
+        severity: 'ERROR',
+        message: 'This is the default display field.',
+        // Log viewer accesses 'component' as 'jsonPayload.component'.
+        component: 'arbitrary-property'
+    },
+    {}
+)
+
+console.error(JSON.stringify(entry))
+
+// Serialize to a JSON string and output.
+console.log(JSON.stringify(entry))
+
 export const logger = createLogger({
     level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
     transports: [
--- src/services/StorageService.ts
+++ src/services/StorageService.ts
@@ -9,9 +9,13 @@ export class StorageService {
         this.storage = new Storage()
     }
 
-    public async getDotenv(bucketName: string): Promise<Buffer> {
-        const secretsFile = this.storage.bucket(bucketName).file('.env')
-        const buffers = await secretsFile.download()
-        return buffers[0]
+    public async getDotenv(bucketName: string): Promise<Buffer | void> {
+        try {
+            const secretsFile = this.storage.bucket(bucketName).file('.env')
+            const buffers = await secretsFile.download()
+            return buffers[0]
+        } catch (e) { 
+            console.error(e)
+        }
     }
 }
